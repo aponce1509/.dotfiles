e7cc9c59f473e7d27af974bf8513a895a0d621a4
aponce1509 <aponce1509@hotmail.com>
2024-05-31 17:48:38 +0200

    file to test nvim features

---
+++ code_editor_languages_tests/python/example.py
@@ -0,0 +1,18 @@
+from sys import path
+
+
+def hello() -> int:
+    print(path)
+    print("hello")
+    return 1
+
+
+# buen días
+def hola() -> int:
+    return 0
+
+
+a = [-6, 1, 4, 4, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+if __name__ == "__main__":
+    hello()
