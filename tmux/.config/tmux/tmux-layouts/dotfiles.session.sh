5b50565076e761da36d18876505472572f28accd
aponce1509 <aponce1509@hotmail.com>
2024-09-01 18:28:29 +0200

    bug: tmuxifier sessions doesnt load conda env from .env

--- tmux/.config/tmux/tmux-layouts/dotfiles.session.sh
+++ tmux/.config/tmux/tmux-layouts/dotfiles.session.sh
@@ -6,9 +6,16 @@ session_root "~/.dotfiles"
 # argument is given, session name will be based on layout file name.
 if initialize_session "dotfiles"; then
   new_window "nvim"
+  run_cmd "source ~/.zshrc"
+  run_cmd "clear"
   run_cmd "nvim"
   new_window "terminal"
   select_window "terminal"
+  run_cmd "source ~/.zshrc"
+  run_cmd "clear"
+  # run_cmd 'source ~/.local/scripts/python_venv_activate'
+  # run_cmd 'CONDA_BIN_PATH="$(conda info --base)/envs/$(basename $CONDA_DEFAULT_ENV)/bin"'
+  # run_cmd 'PATH=$CONDA_BIN_PATH:$PATH'
 fi
 
 # Finalize session creation and switch/attach to it.
